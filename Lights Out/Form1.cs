8d6a0570b6af2636e5fd302501662e86613abe09
Patrick Speiser <doskir@gmail.com>
2011-08-09 10:14:55 +0200

    Improved win handling.

--- Lights Out/Form1.cs
+++ Lights Out/Form1.cs
@@ -11,22 +11,30 @@ namespace Lights_Out
 {
     public partial class Form1 : Form
     {
+        private Timer timer;
         public Form1()
         {
             InitializeComponent();
             NewGame();
-            
-        }
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-            if (board1.Solved)
-                winLabel.Text = "YOU WIN!";
         }
         public void NewGame()
         {
             board1.Initialize(5,5);
             winLabel.Text = "";
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (board1.Solved)
+            {
+                winLabel.Text = "YOU WIN!";
+                timer.Stop();
+                timer.Dispose();
+            }
         }
 
         private void newGameButton_Click(object sender, EventArgs e)
